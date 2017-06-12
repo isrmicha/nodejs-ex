ac4ae28c0af58833e3969c87ad4925cfe88c0737
Israel <isrmicha@gmail.com>
2017-06-12 00:16:46 -0300

    e

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/pagecount', function (req, res) {
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+      res.send(db.collection('counts').find());
     });
   } else {
     res.send('{ pageCount: -1 }');
